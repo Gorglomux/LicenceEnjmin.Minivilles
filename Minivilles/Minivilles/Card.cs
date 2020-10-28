763b1cab1bae9ae089934dee043e9d56f7792774
Gorglomux <38820478+Gorglomux@users.noreply.github.com>
2020-10-28 15:07:00 +0100

    Ajout d'un constructeur pour Card

--- Minivilles/Minivilles/Card.cs
+++ Minivilles/Minivilles/Card.cs
@@ -4,13 +4,23 @@ using System.Text;
 
 namespace Minivilles
 {
-    class Card
+    public class Card
     {
         public int cout;
         public COULEUR couleur;
         public string nom;
         public string descriptionEffet;
         public List<int> valeurActivation;
-        public CARD_ID id;
+        public CARD_ID ID;
+
+        public Card(int cout, COULEUR couleur, string nom, string descriptionEffet, List<int> valeurActivation, CARD_ID id)
+        {
+            this.cout = cout;
+            this.couleur = couleur;
+            this.nom = nom;
+            this.descriptionEffet = descriptionEffet;
+            this.valeurActivation = valeurActivation;
+            ID = iD;
+        }
     }
 }
