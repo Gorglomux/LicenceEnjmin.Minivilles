1ed6ffeec00cf6f50c34758aca25ba66306c742e
Gorglomux <38820478+Gorglomux@users.noreply.github.com>
2020-10-28 09:34:15 +0100

    Ajout du squelette du code
    Création d'un projet vide et ajout d'un gitignore

---
+++ Minivilles/Minivilles/Program.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Minivilles
+{
+    static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Debug.WriteLine("adzad");
+        }
+    }
+}
