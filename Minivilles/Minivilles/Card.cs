9d4c2ce31f72d25a97eb67bb72d0ce275f48c2b2
JulienTHUAULT <julienthuault@hotmail.fr>
2020-10-28 12:01:56 +0100

    Création du squelette de la classe Card et File.

---
+++ Minivilles/Minivilles/Card.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Minivilles
+{
+    class Card
+    {
+        public int cout;
+        public COULEUR couleur;
+        public string nom;
+        public string descriptionEffet;
+        public List<int> valeurActivation;
+        public EFFET effet;
+    }
+}
---
+++ Minivilles/Minivilles/Pile.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Minivilles
+{
+    class Pile
+    {
+        public enum CARD_ID { };
+        private Dictionary<CARD_ID, int> cartes;
+        public Dictionary<CARD_ID, int> _cartes
+        {
+            get
+            {
+                return cartes;
+            }
+            set
+            {
+                cartes = value;
+            }
+        }
+
+        public void PrendreCarte(CARD_ID ID)
+        {
+            cartes[ID] -= 1;
+        }
+
+    }
+}
