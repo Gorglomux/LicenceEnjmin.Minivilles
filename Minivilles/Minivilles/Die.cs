aa95c800608ac93f5215b3aa5c2536697ce4a511
HTourneur <gougodu94@gmail.com>
2020-10-28 14:19:20 +0100

    Création de la classe Die pour les dés

---
+++ Minivilles/Minivilles/Die.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Minivilles
+{
+    class Die
+    {
+        public int Face;
+        public Random rdn;
+
+        public Die()
+        {
+            rdn = new Random();
+        }
+        public int Lancer()
+        {
+            Face = rdn.Next(1, 7);
+            return Face;
+        }
+    }
+}
